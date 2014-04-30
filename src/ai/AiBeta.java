74c941eb53f1632ec509c7904befaa8ebaeb2aff
Dylan Woythal <woythadc194@potsdam.edu>
2014-04-30 14:51:39 -0400

    Save Point

--- src/ai/AiBeta.java
+++ src/ai/AiBeta.java
@@ -1,10 +1,7 @@
 package ai;
 
 import java.awt.Color;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 import logic.Controller;
 
@@ -12,9 +9,9 @@ import ui.GameButton;
 
 public class AiBeta {
 	
-	@SuppressWarnings("unchecked")
-	private static ArrayList<GameButton>[][] attackablePieces = new ArrayList[ 10 ][ 10 ];
+	private static ArrayList<GameButton>[][] attackablePieces;
 	private static GameButton[][] buttonMatrix;
+	private static double [][] attacks; 
 	private static Object [][] HOLYFUCKMAP = new Object[ 10 ][ 10 ];
 	
 	public AiBeta(){
@@ -28,15 +25,24 @@ public class AiBeta {
 		}
 	}
 	
+	@SuppressWarnings("unchecked")
 	public static void makeMove1(){
+		attacks = new double[ 10 ][ 10 ];
+		attackablePieces = new ArrayList[ 10 ][ 10 ];
 		buttonMatrix = Controller.getButtonMatrix();
 		for( int x=0; x<10; x++ ){
 			for( int y=0; y<10; y++ ){
 				GameButton button = buttonMatrix[ x ][ y ];
 				if( button.getPlayerColor() == Color.RED)
 					attackablePieces[ x ][ y ] = ProbabilityCalculator.findTargets( button, false );
+				if( attackablePieces[ x ][ y ]!= null && !attackablePieces[ x ][ y ].isEmpty() ){
+					double battleStats = 0.0;
+					battleStats += 
+				}
 			}
 		}
+		
+		
 		/*
 		for( int x=0; x<10; x++ )
 			System.out.println( Arrays.toString(attackablePieces[ x ] ) );
